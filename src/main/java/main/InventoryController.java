fc509247728bb43aa434d40c83932d6703549331
well-bluu <kbmaputi@gmail.com>
2025-12-04 16:53:07 +0800

    Added New Stock button functions
    New stock button can now be used. Functions include:
    
    - Being able to choose the type of meal from database
    - Instructions
    - Type the stock
    - Choose Status

--- src/main/java/main/InventoryController.java
+++ src/main/java/main/InventoryController.java
@@ -11,6 +11,9 @@ import java.sql.SQLException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
@@ -18,7 +21,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
+
 
 /**
  * Controller for the Inventory page.
@@ -275,11 +281,27 @@ public class InventoryController {
 
     /**
      * Handles the "New Stock" button click.
-     * TODO: Opens a dialog to add a new inventory item.
+     * Opens a dialog to add a new inventory item using FXML.
      */
     @FXML
     private void handleNewStock() {
         System.out.println("New Stock button clicked");
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/newstock-dialog.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Add New Stock");
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+
+            // Refresh the table after dialog closes
+            loadInventoryData();
+        } catch (Exception e) {
+            System.out.println("[InventoryController] ERROR loading dialog: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -297,7 +319,7 @@ public class InventoryController {
         else {
         ObservableList<InventoryItem> filteredList = FXCollections.observableArrayList();
 
-        for(InventoryItem item : masterObservableList) 
+            for(InventoryItem item : masterObservableList) 
                 if (item.getStatus().toLowerCase().equals(currentFilter.toLowerCase())) {
                     filteredList.add(item);
                 }
